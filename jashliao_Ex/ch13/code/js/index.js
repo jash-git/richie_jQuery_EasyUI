39a50691fa5aeb8c97a2a6bdc9d9af94afd80bb4
Gitea <gitea@fake.local>
2021-04-01 14:51:40 +0800

    Add 202104011451

---
+++ jashliao_Ex/ch13/code/js/index.js
@@ -0,0 +1,79 @@
+$(function () {
+	
+	$('#box').accordion({
+		//width : 500,
+		//height : 300,
+		//fit : true,
+		//border : false,
+		//animate : false,
+		//multiple : true,
+		//selected : 1,
+		/*
+		onSelect : function (title, index) {
+			alert(title + '|' + index);
+		},
+		onUnselect : function (title, index) {
+			alert(title + '|' + index);
+		},
+		
+		onAdd : function (title, index) {
+			alert(title + '|' + index);
+		},
+		
+		onBeforeRemove : function (title, index) {
+			alert('before' + title + '|' + index);
+		},
+		onRemove : function (title, index) {
+			alert('' + title + '|' + index);
+		},
+		
+		*/
+	})
+	
+	//console.log($('#box').accordion('panels'));
+	//console.log($('#box').accordion('panels'));
+	/*
+	$(document).click(function () {
+		$('#box').accordion().show();
+		$('#box').accordion('resize');
+	});
+	
+	
+	//console.log($('#box').accordion('getSelected'));
+	$(document).click(function () {
+		console.log($('#box').accordion('getSelections'));
+	});
+	*/
+	
+	//console.log($('#box').accordion('getPanel', 1));
+	//console.log($('#box').accordion('getPanelIndex', '#a2'));
+	//$('#box').accordion('select', 1);
+	//$('#box').accordion('unselect', 0);
+	/*
+	$('#box').accordion('add', {
+		title : '新面板',
+		closable : true,
+		content : '新内容',
+		onClose : function () {
+			alert('close');
+		},
+		//selected : false,
+		//collapsible : false,
+	});
+	*/
+	
+	//$('#box').accordion('remove', 0);//移除第一個選單
+});
+
+
+
+	
+
+
+
+
+
+
+
+
+
