2b7e48a6faede99c2aeb50e49132478a2541ec1e
Gitea <gitea@fake.local>
2021-04-08 11:12:27 +0800

    Add 202104081112

---
+++ jashliao_Ex/ch16/code/js/index.js
@@ -0,0 +1,53 @@
+$(function () {
+
+
+	$('#box').dialog({
+		width : 400,
+		height : 250,
+		title : '对话框',
+		modal : true,
+		/*
+		collapsible : true,
+		minimizable : true,
+		maximizable : true,
+		resizable : true,
+		*/
+		//toolbar : '#tt',
+		toolbar : [{
+			text : '编辑',
+			iconCls : 'icon-edit',
+			handler : function () {
+				alert('edit');
+			}
+		},{}],
+		buttons : [{
+			text : '确定',
+			plain : true,
+			iconCls : 'icon-ok',
+		},{
+			text : '取消',
+			plain : true,
+			iconCls : 'icon-cancel',
+		}],
+		onClose : function () {
+			alert('关闭时触发！');
+		}
+	});
+
+	
+	console.log($('#box').dialog('dialog'));
+
+});
+
+
+
+	
+
+
+
+
+
+
+
+
+
